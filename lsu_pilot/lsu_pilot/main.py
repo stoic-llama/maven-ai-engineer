99fac0538e55ac5d422b37c08bd0f6197f8fc25b
John Liu <stoic.llama@gmail.com>
2024-05-04 17:48:43 -0400

    Initial Commit

---
+++ lsu_pilot/lsu_pilot/main.py
@@ -0,0 +1,60 @@
+import os
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from dotenv import load_dotenv
+from openai import OpenAI
+
+
+load_dotenv()  # take environment variables from .env.
+
+tg_bot_token = os.getenv("TG_BOT_TOKEN")
+openai = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+
+messages = [{
+  "role": "system",
+  "content": "You are a helpful assistant that answers questions."
+}]
+
+logging.basicConfig(
+  format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+  level=logging.INFO)
+
+'''
+1. UPDATE: object that contains all the information and data 
+that are coming from Telegram itself (like the message, 
+the user who issued the command, etc) 
+
+2. CONTEXT: object that contains information and data about 
+the status of the library itself (like the Bot, the Application, 
+the job_queue, etc).
+
+SOURCE: https://github.com/python-telegram-bot/python-telegram-bot/wiki/Extensions---Your-first-Bot
+'''
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+  await context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text="I'm a bot, please talk to me!")
+
+async def chat(update: Update, context: ContextTypes.DEFAULT_TYPE):
+  messages.append({"role": "user", "content": update.message.text})
+  completion = openai.chat.completions.create(model="gpt-3.5-turbo",
+                                              messages=messages)
+  completion_answer = completion.choices[0].message
+  messages.append(completion_answer)
+
+  await context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text=completion_answer.content)
+
+
+if __name__ == '__main__':
+  application = ApplicationBuilder().token(tg_bot_token).build()
+
+  start_handler = CommandHandler('start', start)
+  chat_handler = CommandHandler('chat', chat) #new chat CommandHandler created
+
+  application.add_handler(start_handler)
+  application.add_handler(chat_handler) # New chat handler connected
+
+  application.run_polling()
+
